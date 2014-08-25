44223995d7d2cab564c9e6b286a8b724ebf37c47
Malobre <l.omdumorbhian@gmail.com>
2014-08-25 18:08:15 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define ('BASE_DIR', __DIR__);
+  define ('UPLOAD_DIR', BASE_DIR . 'uploads/');
+  define ('BASE_URL', 'http://image.mywebsite.com');
+  define ('ALLOWED_KEYS', 'myKey,friendKey,anotherFriendKey');
+  define ('ALLOWED_EXTS_DOWNLOAD', 'jpg,jpeg,png,gif,bmp');
+?>
