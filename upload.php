2dbcd789e93785225f7325346731ecc5771732c9
Malobre <l.omdumorbhian@gmail.com>
2014-08-26 15:54:18 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,20 +1,26 @@
 <?php
     include('config.php');
-
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['file']) && isset($_POST['key'])) {
+	
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES["file"]) && isset($_POST['key'])) {
         
-        $file = $_POST['file'];
-        if(!$file['size'] > MAX_FILE_SIZE && array_search($_POST['key'], explode(",", ALLOWED_KEYS))) {
-            $ext = strtolower(end(explode('.', $file['name'])));
+        $file = $_FILES["file"];
+		
+        if($file['size'] <= MAX_FILE_SIZE && array_search($_POST['key'], explode(",", ALLOWED_KEYS)) !== false) {
+            $ext = explode('.', $file['name']);
+			$ext = strtolower(end($ext));
             
-            $generated_name = "";
+            $generated_name = substr(md5(uniqid(rand(), true)), 0, 4);
             do{
                 $generated_name .= substr(md5(uniqid(rand(), true)), 0, rand(2, 5));
-            }while(file_exists(UPLOAD_DIR.$name.'.'.$ext));
+            }while(file_exists(UPLOAD_DIR.$generated_name.'.'.$ext));
             
             move_uploaded_file($file['tmp_name'], UPLOAD_DIR.$generated_name.'.'.$ext);
             
             echo BASE_URL.$generated_name;
-        }
-    }
+        } else {
+			echo "ERROR FILE TOO BIG OR WRONG KEY ";
+		}
+    } else {
+		echo "ERROR NO FILE SENDED OR NO KEY";
+	}
 ?>
