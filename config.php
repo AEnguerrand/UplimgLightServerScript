01d0f2048d09e2b1c5a8b1acb7d91aae5c7a016e
Malobre <l.omdumorbhian@gmail.com>
2014-08-26 17:04:50 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     define ('BASE_DIR', __DIR__.'/');
     define ('UPLOAD_DIR', BASE_DIR . 'uploads/');
-    define ('BASE_URL', 'http://109.190.43.164/UplimgLightServerScript-master/');
+    define ('BASE_URL', 'http://image.mywebsite.fr/');
     define ('ALLOWED_KEYS', 'myKey,friendKey,anotherFriendKey');
     define ('ALLOWED_EXTS_DOWNLOAD', 'jpg,jpeg,png,gif,bmp');
     
