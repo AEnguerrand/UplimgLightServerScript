e99ab25a8b7cc9afeb4efe190b4c5850191bf6f2
Malobre <l.omdumorbhian@gmail.com>
2014-08-26 17:12:54 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,10 +1,10 @@
 <?php
     include('config.php');
-	
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES["file"]) && isset($_POST['key'])) {
+    
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES["uplimgFile"]) && isset($_POST['key'])) {
         
-        $file = $_FILES["file"];
-		
+        $file = $_FILES["uplimgFile"];
+        
         if($file['size'] <= MAX_FILE_SIZE && array_search($_POST['key'], explode(",", ALLOWED_KEYS)) !== false) {
             $ext = explode('.', $file['name']);
             $ext = strtolower(end($ext));
