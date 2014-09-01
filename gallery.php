4feed0f3c0ebcb5dfb63f899400118603af80173
Engue0774 - Enguerrand <contact@engue0774.fr>
2014-09-01 11:18:50 +0200

    Test

--- gallery.php
+++ gallery.php
@@ -5,25 +5,24 @@
     </head>
     <body>
         <ul>
-<?php
-    include('config.php');
-    
-    if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['key'])) {
-        if(array_search($_GET['key'], explode(",", ALLOWED_KEYS)) !== false) {
-            if($folder = opendir(UPLOAD_DIR)) {
-                while(false !== ($file = readdir($folder))) {
-                    if($file != '.' && $file != '..' && $file != '.htaccess') {
-                        $ext = explode('.', $file);
-                        $ext = strtolower(end($ext));
-                        $file = str_replace(".".$ext, "", $file);
-                        echo "<li><a href='".BASE_URL.$file."'>".BASE_URL.$file."</a></li>";
+            <?php
+            include('config.php');
+
+            if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['key'])) {
+                if (array_search($_GET['key'], explode(",", ALLOWED_KEYS)) !== false) {
+                    if ($folder = opendir(UPLOAD_DIR)) {
+                        while (false !== ($file = readdir($folder))) {
+                            if ($file != '.' && $file != '..' && $file != '.htaccess') {
+                                $ext = explode('.', $file);
+                                $ext = strtolower(end($ext));
+                                $file = str_replace("." . $ext, "", $file);
+                                echo "<li><a href='" . BASE_URL . $file . "'>" . BASE_URL . $file . "</a></li>";
+                            }
+                        }
                     }
                 }
             }
-        }
-        
-    }
-?>
+            ?>
         </ul>
     </body>
 </html>
